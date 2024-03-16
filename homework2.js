e3da2a63f5863ec6137a92f888bf8655f35dac89
julia <naymovaj@gmail.com>
2024-03-16 18:43:19 +0100

    3 tasks were finished in homework2

--- homework2.js
+++ homework2.js
@@ -1,3 +1,4 @@
+// task 1
 function grades(points) {
   if (points >= 90 && points <= 100) {
     return 'perfect';
@@ -9,3 +10,36 @@ function grades(points) {
     return 'You can do better';
   }
 }
+console.log(grades(99));
+
+// task 2
+
+function calculator(operator, a, b) {
+  let result = 'calculation error';
+  switch (operator) {
+    case 'add':
+      result = a + b;
+      break;
+
+    case 'substract':
+      result = a - b;
+      break;
+    case 'multiply':
+      result = a * b;
+      break;
+    case 'divide':
+      result = a / b;
+      break;
+  }
+
+  return result;
+}
+console.log('We recieve  ' + calculator('divide', 171, 2));
+
+// task 3
+
+function comparison(num) {
+  let result = num % 2 == 0 ? 'even number' : 'odd number';
+  return result;
+}
+console.log(comparison(4));
