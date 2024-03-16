986a0af02eef65134080cc1621201e18b39c280e
julia <naymovaj@gmail.com>
2024-03-16 17:13:55 +0100

    Added first function

---
+++ homework2.js
@@ -0,0 +1,12 @@
+function grades(points) {
+  if (points >= 90 && points <= 100) {
+    return 'perfect';
+  } else if (points >= 70 && points <= 89) {
+    return 'excelent';
+  } else if (points >= 50 && points <= 69) {
+    return 'good';
+  } else if (points < 50) {
+    return 'You can do better';
+  }
+}
+console.log(grades(95));
